02b508b2164b99a7062a091358d64cbede616fad
Catherine <cbronzo@mac.com>
2019-02-27 16:51:57 -0800

    Stubbed out Post Controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new post_params
+    if @post.save
+      redirect_to posts_path, notice: 'Post created'
+    else
+      Rails.logger.info @post.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to post_path(@post), notice: "Post Updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to posts_path
+    else
+      redirect_to post_path(@post), error: 'Could not destroy'
+    end
+  end
+
+  protected
+
+  def set_post
+    @post = Post.find params[:id]
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description, :due_date)
+  end
+
+end
