1f138441ce4ec27fa0d2c7b293362bfddd299d5c
Catherine <cbronzo@mac.com>
2019-02-28 15:14:31 -0800

    Created new, create and user_params

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,24 @@
 class UserController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:message] = "Successfully Signed Up!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+
 end
