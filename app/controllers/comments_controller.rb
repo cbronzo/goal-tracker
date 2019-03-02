e72f3de0494059ddc747f10570e25ad2f5d91419
Catherine <cbronzo@mac.com>
2019-03-01 16:48:42 -0800

    Added create and params methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
+require 'pry'
+
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    comment = Comment.new comment_params.merge(post_id: params[:post_id], user_id: params[:user_id])
+    if comment.save
+      flash[:notice] = 'Created'
+    else
+      flash[:error] = 'Cound not save'
+    end
+    redirect_to user_post_path(params[:user_id], params[:post_id])
+  end
+
+  protected
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :user_id, :comment_text)
+  end
 end
