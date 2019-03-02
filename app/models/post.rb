d483785d4dd17e9e6b5f8eea49a1b02a4148538a
Catherine <cbronzo@mac.com>
2019-03-01 19:32:11 -0800

    Added test validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,13 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
+  # validates :title, :minimum => 10, allow_blank: false
+  # validates :description, :minimum => 20, allow_blank: false
+  # validates :due_date, allow_blank: true
+  # validates :due_date, :type => Date
+
+
+
 
 
 
