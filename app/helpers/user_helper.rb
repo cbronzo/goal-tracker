f905afe068cf5cfb6903205a7eb129c6d581422c
Catherine <cbronzo@mac.com>
2019-03-05 13:11:39 -0800

    Added link helper method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,10 @@
 module UserHelper
+
+  def posts_link
+    if params[:user_id].present?
+      link_to 'View All Goals', posts_path
+    else
+      link_to 'View My Goals!', user_posts_path(current_user.id)
+    end
+  end
 end
