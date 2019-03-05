6139af0ce62a9216bbdd258160ffcb41815f9a74
Catherine <cbronzo@mac.com>
2019-03-05 13:12:39 -0800

    Added mapping & display for selection menu

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,26 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :title, presence: true, length: { minimum: 8 }
-  validates :description, presence: true, length: { minimum: 20, too_short: "Description too short" }
+  validates :description, presence: true, length: { minimum: 20, too_short: "too short" }
 
+  DIFFICULTY_MAPPING = {
+    1 => 'Very Easy',
+    2 => 'Easy',
+    3 => 'Somewhat Easy',
+    4 => 'Somewhat Difficult',
+    5 => 'Difficult',
+    6 => 'Very Difficult'
+  }
 
+  def self.difficulty_mapping
+    a = []
+    Post::DIFFICULTY_MAPPING.each {|k, v| a << [v, k]  }
+    a
+  end
 
+  def difficulty_to_str
+    Post::DIFFICULTY_MAPPING.select {|k, v| k == difficulty}.values.first
+  end
 
 
 
