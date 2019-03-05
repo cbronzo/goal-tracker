c37a80cd86ebff913e274f8215d0033c7f8a8c1e
Catherine <cbronzo@mac.com>
2019-03-05 13:14:29 -0800

    Updated create & comment_params to get current_user id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,19 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    comment = Comment.new comment_params.merge(post_id: params[:post_id], user_id: params[:user_id])
+    comment = Comment.new comment_params.merge(post_id: params[:post_id], user_id: current_user.id)
     if comment.save
-      flash[:notice] = 'Success!'
+      flash[:notice] = 'Comment added'
     else
       flash[:error] = 'Unable to post comment. Please try again.'
     end
-    redirect_to user_post_path(params[:user_id], params[:post_id])
+    Rails.logger.info comment.errors.full_messages
+    redirect_to user_post_path(current_user.id, params[:post_id])
   end
 
   protected
 
   def comment_params
-    params.require(:comment).permit(:post_id, :user_id, :comment_text)
+    params.require(:comment).permit(:post_id, :comment_text)
   end
 end
