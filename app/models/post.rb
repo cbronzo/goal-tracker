51b835d14e00b0e31c654e43f417b6fa7758006a
Catherine <cbronzo@mac.com>
2019-03-06 21:39:55 -0800

    Added mapping and methods for goal attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 8 }
   validates :description, presence: true, length: { minimum: 20, too_short: "too short" }
 
+  
+
   DIFFICULTY_MAPPING = {
     1 => 'Very Easy',
     2 => 'Easy',
@@ -16,6 +18,32 @@ class Post < ApplicationRecord
     5 => 'Difficult',
     6 => 'Very Difficult'
   }
+
+  PRIORITY_MAPPING = {
+    1 => 'Very Low Priorty',
+    2 => 'Low Priority',
+    3 => 'Medium Priority',
+    4 => 'High Priority',
+    5 => 'Very High Priority'
+  }
+
+  PROGRESS_MAPPING = {
+    1 => 'Stuggling...',
+    2 => 'Just Starting!',
+    3 => 'Making Progress!',
+    4 => 'Halfway There!',
+    5 => 'Almost Done!',
+    6 => 'Completed!'
+  }
+
+
+  def cheer(user)
+    cheers << Cheer.new(user: user)
+  end
+
+  def uncheer(user)
+    cheers.where(user_id: user.id).first.destroy
+  end
 
   def self.difficulty_mapping
     a = []
@@ -27,8 +55,25 @@ class Post < ApplicationRecord
     Post::DIFFICULTY_MAPPING.select {|k, v| k == difficulty}.values.first
   end
 
+  def self.priority_mapping
+    a = []
+    Post::PRIORITY_MAPPING.each {|k, v| a << [v, k]  }
+    a
+  end
 
+  def priority_to_str
+    Post::PRIORITY_MAPPING.select {|k, v| k == priority}.values.first
+  end
 
+  def self.progress_mapping
+    a = []
+    Post::PROGRESS_MAPPING.each {|k, v| a << [v, k]  }
+    a
+  end
+
+  def progress_to_str
+    Post::PROGRESS_MAPPING.select {|k, v| k == progress}.values.first
+  end
 
 
 
