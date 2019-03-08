85ef6f79f3b1f58fd719f38dab11f521660198fb
Catherine <cbronzo@mac.com>
2019-03-07 23:28:04 -0800

    Added validation & javascript for cheer

--- app/models/cheer.rb
+++ app/models/cheer.rb
@@ -2,4 +2,6 @@ class Cheer < ApplicationRecord
   belongs_to :post
   belongs_to :user
   belongs_to :cheerleader, class_name: 'User', foreign_key: 'user_id'
+
+  validates :user_id, uniqueness: {scope: :post_id}
 end
