e90e808a6f7e00622a960a6107934c8b0fa30cac
Catherine <cbronzo@mac.com>
2019-03-07 23:27:25 -0800

    Added helper method to sort posts

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -7,4 +7,12 @@ module UserHelper
       link_to 'View My Goals!', user_posts_path(current_user.id)
     end
   end
+
+  def sorted_posts_link(sort_by)
+    if params[:user_id].present?
+      link_to sort_by, user_posts_path(current_user.id, sort_by: sort_by.downcase)
+    else
+      link_to sort_by, posts_path(sort_by: sort_by.downcase)
+    end
+  end
 end
