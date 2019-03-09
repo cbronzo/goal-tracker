00d8a3b854f5ec81cafb17ba462d0dfb281f64fa
Catherine <cbronzo@mac.com>
2019-03-09 11:56:50 -0800

    Added .humanize to sorted_posts_link method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,17 +2,19 @@ module UserHelper
 
   def posts_link
     if params[:user_id].present?
-      link_to 'View All Goals', posts_path
+      link_to 'View All Goals', root_path
     else
-      link_to 'View My Goals!', user_posts_path(current_user.id)
+      link_to 'View My Goals', user_posts_path(current_user.id)
     end
   end
 
-  def sorted_posts_link(sort_by)
-    if params[:user_id].present?
-      link_to sort_by, user_posts_path(current_user.id, sort_by: sort_by.downcase)
-    else
-      link_to sort_by, posts_path(sort_by: sort_by.downcase)
-    end
+  def sorted_posts_link(sort_by, link_text = nil)
+  text = link_text || sort_by.humanize
+
+  if params[:user_id].present?
+    link_to text, user_posts_path(current_user.id, sort_by: sort_by.downcase)
+  else
+    link_to text, posts_path(sort_by: sort_by.downcase)
   end
+end
 end
